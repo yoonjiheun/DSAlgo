c1c934d592f452e9ab68e6839ffd34e50dd26c84
John <yoonjiheun28@gmail.com>
2017-09-02 15:03:35 -0400

    Doubly Linked List. Need to add implementation for interface methods

--- src/dsalgo/list/linked/DoublyLinkedList.java
+++ src/dsalgo/list/linked/DoublyLinkedList.java
@@ -1,5 +1,105 @@
 package dsalgo.list.linked;
 
-public class DoublyLinkedList {
 
+
+import dsalgo.list.List;
+import dsalgo.node.DoublyNode;
+import dsalgo.node.SinglyNode;
+
+public class DoublyLinkedList implements List {
+	DoublyNode head;
+	DoublyNode tail;
+	DoublyNode currPos;
+	int currSize;
+	
+	public DoublyLinkedList() {
+		tail = new DoublyNode();
+		tail.setElement(null);
+		head = new DoublyNode();
+		head.setElement(null);
+		
+		head.setNext(tail);
+		head.setPrev(null);
+		tail.setPrev(head);
+		tail.setNext(null);
+		
+		currPos = tail;
+		
+		currSize = 0;
+	}
+	
+	public boolean insert(Object it) {
+		DoublyNode node = new DoublyNode(it, currPos.prev(), currPos);
+		currPos = node;
+		currPos.prev().setNext(currPos);
+		currPos.next().setPrev(currPos);
+		currSize++;
+		
+		return true;
+	}
+	
+	public boolean append(Object it) {
+		tail.setPrev(new DoublyNode(it, tail.prev(), tail));
+		tail.prev().prev().setNext(tail.prev());
+		if(currPos == tail) currPos = tail.prev();
+		currSize++;
+		return true;
+	}
+	
+	public Object remove() {
+		if(currPos == tail) return null;
+		Object objectToRemove = currPos;
+		currPos.next().setPrev(currPos.prev());
+		currPos.prev().setNext(currPos.next());
+		currPos = currPos.next();
+		currSize--;
+		return objectToRemove;
+	}
+	
+	
+
+	public void moveToStart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void moveToEnd() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void prev() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int currPos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public String toString() {
+		String value = "0 = head\n";
+		DoublyNode currPos = head;
+		
+		int c = 0;
+		while(currPos != tail) {
+			value += c + ": " + currPos.element() + " (curr pos value)  " + (c+1) + ": " + (currPos.next() != null ? currPos.next().element() : null)  + " (next pos value)   " + (c-1) + ": " + (currPos.prev() != null ? currPos.prev().element() : null) + " (prev pos value)\n";
+			currPos = currPos.next();
+			c++;
+		}
+		
+		return "Size: " + currSize + "\nValue: \n" + value;
+	}
 }
--- src/dsalgo/test/Main.java
+++ src/dsalgo/test/Main.java
@@ -1,18 +1,25 @@
 package dsalgo.test;
 
 import dsalgo.list.array.ArrayList;
+import dsalgo.list.linked.DoublyLinkedList;
 import dsalgo.list.linked.LinkedList;
 
 public class Main {
+	// TODO: Write unit tests.
+	// Just checking for null pointers with main method.
 	public static void main(String[] args) {
 		ArrayList list = new ArrayList(20, false);
 		LinkedList linkedList = new LinkedList();
+		DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
 		for(int i=0; i<45;i++) {
 			String rng = String.format("%.0f", i+1 * Math.random() * 100);
 			list.append(rng);
 			linkedList.append(rng);
+			doublyLinkedList.append(rng);
 		}
 		
+		System.out.println(list.toString());		
+		System.out.println(doublyLinkedList.toString());		
 		System.out.println(linkedList.toString());
 	}
 }
